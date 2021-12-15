ea452eb3374650b95aae9b753a34234572fe94a7
ADEEL ASGHAR <adeelasghar9292@gmail.com>
2021-12-15 15:33:19 +0500

    db connection

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 export default {
   PORT: process.env.PORT || 5000,
-  MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost:27017/amazona',
+  MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://ecommerence:<password>@cluster1.1umis.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
   accessKeyId: process.env.accessKeyId || 'accessKeyId',
